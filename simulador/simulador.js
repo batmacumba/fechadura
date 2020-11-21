a215c1012047ed4230fb504eb33d7a52af0e85b7
batmacumba <brunocarneirodacunha@usp.br>
2020-11-21 18:47:21 -0300

    add: gitignore & simulador & docker-compose

--- simulador/simulador.js
+++ simulador/simulador.js
@@ -4,15 +4,43 @@
  servidor MQTT seguindo um padrão JSON
  ******************************************************************************/
 
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var crypto = require('crypto');
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://127.0.0.1')
+var current = [];
+
+// client.on('connect', function () {
+// 	client.subscribe('presence')
+// 	client.publish('presence', 'Hello mqtt')
+// })
  
-client.on('connect', function () {
-	client.publish('presence', 'Hello mqtt')
-})
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+// client.on('message', function (topic, message) {
+//   // message is Buffer
+//   console.log(message.toString())
+//   client.end()
+// })
+function someoneComesIn() {
+	var random = Math.random().toString();
+	var id = crypto.createHash('sha256').update(random).digest('hex');
+	var message = '{ "id": "' + id + '", "timestamp": "' + new Date().toISOString() + '" }';
+	current.push(id);
+	client.publish('entrada', message);
+	client.publish('current', current.toString());
+}
+
+function someoneComesOut() {
+	if (current.length === 0) return;
+	var random = Math.floor(Math.random() * current.length);
+	var id = current.splice(random, 1)[0];
+	var message = '{ "id": "' + id + '", "timestamp": "' + new Date().toISOString() + '" }';
+	client.publish('saiu', message);
+	client.publish('current', current.toString());
+}
+
+function coinToss() {
+	if (Math.random() < 0.5) someoneComesIn();
+	else someoneComesOut();
+	console.log(current);
+}
+
+setInterval(coinToss, 1000);

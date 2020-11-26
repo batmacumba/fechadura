79bb37a001813a0dca09870c217903f22670ab87
batmacumba <brunocarneirodacunha@usp.br>
2020-11-26 00:04:08 -0300

    chg: fechadura ok

--- fechadura/fechadura.ino
+++ fechadura/fechadura.ino
@@ -5,86 +5,90 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
-#include "bag.h"
+#include "list.h"
 
 #define RST_PIN           D0  
 #define SS_PIN            D8  
 #define MISO_PIN          D6 
 #define MOSI_PIN          D7 
 #define SCK_PIN           D5  
-#define STASSID           "Obi Wan Kenobi_IoT"
-#define STAPSK            "oladobomdaforca" // minha senha do wifi, uhuu
-#define MSG_BUFFER_SIZE   (50)
-#define MAX_OCUPANTES     128
-#define NTP_OFFSET        -3 * 60 * 60      // In seconds
-#define NTP_INTERVAL      60 * 1000    // In miliseconds
+#define SSID              "Obi Wan Kenobi_IoT"
+#define PASS              "oladobomdaforca"       // minha senha do wifi, uhuu
+#define MQTT_SERVER       "192.168.15.49"
+#define MAX_BUFFER_SIZE   256
+#define NTP_OFFSET        -3 * 60 * 60            // In seconds
+#define NTP_INTERVAL      60 * 1000               // In miliseconds
 #define NTP_ADDRESS       "europe.pool.ntp.org"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
-const char* ssid     = STASSID;
-const char* password = STAPSK;
-const char* mqtt_server = "192.168.15.49";
-unsigned long lastMsg = 0;
-char msg[MSG_BUFFER_SIZE];
-String authorizedIDs = "";
-int value = 0;
-char *current[MAX_OCUPANTES];
-bool currentNotRestored = true;
+WiFiUDP ntpUDP; 
+NTPClient time_client(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
+bool current_not_restored = true;
+List *authorized = list_new();
+List *current = list_new();
+char uid[MAX_BUFFER_SIZE];
 
-String 
-dump_byte_array(byte *buffer, byte bufferSize) 
+int
+str_cmp(void *a, void *b)
 {
-    String rfid_uid = "";
-    for (byte i = 0; i < bufferSize; i++) {
-        String uid_part = String(buffer[i], HEX); 
-        rfid_uid += uid_part;
-    }
-    return rfid_uid;
+    char *str_a = (char *) a;
+    char *str_b = (char *) b;
+    return strcmp(str_a, str_b);
 }
 
-String
+void
 readRFID()
 {
-    if (!mfrc522.PICC_IsNewCardPresent()) {
-      delay(50);
-      return "";
+    static unsigned long last_new_reading = 0;
+    static char last_new_uid[MAX_BUFFER_SIZE];
+    String str_uid = "";
+
+    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+        goto zera_uid;
+        
+    for (byte i = 0; i <  mfrc522.uid.size; i++) {
+        String uid_part = String(mfrc522.uid.uidByte[i], HEX); 
+        str_uid += uid_part;
     }
-    if (!mfrc522.PICC_ReadCardSerial()) {
-      delay(50);
-      return "";
+    strncpy(uid, str_uid.c_str(), MAX_BUFFER_SIZE);
+    // Leitura repetida
+    if (strcmp(uid, last_new_uid) == 0 && millis() - last_new_reading < 3000)
+        goto zera_uid;
+    // Nova leitura
+    else {
+        strncpy(last_new_uid, uid, MAX_BUFFER_SIZE);
+        last_new_reading = millis();
+        return;
     }
-    String rfid_uid = dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-    return rfid_uid;
+    
+    zera_uid:
+        uid[0] = '\0';
 }
 
 void 
 setup_wifi() 
 {
 
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASS);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void 
@@ -94,31 +98,39 @@ callback(char* topic, byte* payload, unsigned int length)
     for (int i = 0; i < length; i++) {
         message += (char) payload[i];
     }
-    if (strncmp(topic, "autorizados", 11) == 0) authorizedIDs = message;
+    if (strncmp(topic, "autorizados", 11) == 0) update_authorized(message);
     if (strncmp(topic, "ocupacao", 8) == 0) {
-        restoreCurrent(message);
-        currentNotRestored = false;
+        restore_current(message);
+        current_not_restored = false;
     }
 }
 
 void 
 reconnect() 
 {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "ESP8266Client-";
+    String clientId = "Fechadura-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      client.subscribe("autorizados");
-      client.subscribe("ocupacao");
-      // TODO: possível bug: na primeira execução a mensagem ocupacao não chegará e
-      // a execução não sairá daqui
-      while (currentNotRestored) client.loop();
-      client.unsubscribe("ocupacao");
+        Serial.println("connected");
+        client.subscribe("autorizados");
+        client.subscribe("ocupacao");
+
+        unsigned long start = millis();
+        unsigned long now = millis();
+        while (current_not_restored) {
+            client.loop();
+            if (now - start > 2000) {
+                Serial.println("Could not restore state from MQTT server! Trying again...");
+                now = millis();
+            }
+            // Start from scratch
+            if (millis() - start > 10000) break;
+        }
+        client.unsubscribe("ocupacao");
     } 
     else {
       Serial.print("failed, rc=");
@@ -130,15 +142,22 @@ reconnect()
   }
 }
 
-bool 
-isAuthorizedID(String rfid_uid)
+void
+update_authorized(String message)
 {
-    if (authorizedIDs.indexOf(rfid_uid) > 0) return true;
-    return false;
+    char buffer[MAX_BUFFER_SIZE];
+    strncpy(buffer, message.c_str(), MAX_BUFFER_SIZE);
+    // Limpa aspas antes e depois
+    buffer[strlen(buffer) - 1] = '\0';
+    char *token = strtok(buffer + 1, ", ");
+    while (token != NULL) {
+        list_append(authorized, token, strlen(token) + 1);
+        token = strtok(NULL, ", ");
+    }
 }
 
 void
-restoreCurrent(String message)
+restore_current(String message)
 {
     StaticJsonDocument<200> doc;
     DeserializationError error = deserializeJson(doc, message.c_str());
@@ -148,85 +167,25 @@ restoreCurrent(String message)
         return;
     }
     const char *ocupantes = doc["ocupantes"];
-    char buffer[256];
-    // TODO: buffer overflow
-    strcpy(buffer, ocupantes);
-    int i = 0;
+    char buffer[MAX_BUFFER_SIZE];
+    strncpy(buffer, ocupantes, MAX_BUFFER_SIZE);
     char *token = strtok(buffer, ", ");
     while (token != NULL) {
-        strncpy(current[i++], token, 9);
+        list_append(current, token, strlen(token) + 1);
         token = strtok(NULL, ", ");
     }
 }
 
-bool
-notInCurrent(String rfid_uid)
-{
-    for (int i = 0; i < MAX_OCUPANTES; i++) 
-        if (strncmp(current[i], rfid_uid.c_str(), 8) == 0)  return false;
-    return true;
-}
-
 void
-addToCurrent(String rfid_uid)
-{
-    for (int i = 0; i < MAX_OCUPANTES; i++) {
-        if (strlen(current[i]) == 0) {
-            strncpy(current[i], rfid_uid.c_str(), 9);
-            return;
-        }
-    }
-}
-
-void
-shiftCurrent()
-{
-    for (int i = 0; i < MAX_OCUPANTES; i++) {
-        // Troca o zero no i pelo ID no i+1 e zera o i+1
-        if (strlen(current[i]) == 0 && strlen(current[i + 1]) > 0) {
-            strncpy(current[i], current[i + 1], 9);
-            *current[i + 1] = '\0';
-        }
-        // Dois zeros seguidos significa que não é mais necessário continuar o shift
-        if (strlen(current[i]) == 0 && strlen(current[i + 1]) == 0) 
-            return;
-    }
-}
-
-int
-removeFromCurrent(String rfid_uid)
-{
-    for (int i = 0; i < MAX_OCUPANTES; i++) {
-        if (strncmp(current[i], rfid_uid.c_str(), 8) == 0) {
-            *current[i] = '\0';
-            // Vamos evitar "buracos" no array
-            shiftCurrent();
-            return 0;
-        }
-    }
-    return -1;
-}
-
-void
-initializeCurrent()
-{
-    for (int i = 0; i < MAX_OCUPANTES; i++) {
-        current[i] = (char *) malloc(sizeof(char) * 9);
-        *current[i] = '\0';
-    }
-}
-
-String
-formatCurrent()
+format_current(char *buffer)
 {
     String formatted = "";
-    for (int i = 0; i < MAX_OCUPANTES; i++) {
-        if (strlen(current[i]) > 0) {
-          if (strlen(current[i + 1]) == 0 || i == MAX_OCUPANTES - 1) formatted += String(current[i]);
-          else formatted += String(current[i]) + ", "; 
-        }
+    for (int i = 0; i < list_length(current); i++) {
+        char *str = (char *) list_get(current, i);
+        if (i == list_length(current) - 1) formatted += String(str);
+        else formatted += String(str) + ", "; 
     }
-    return formatted;
+    strcpy(buffer, formatted.c_str());
 }
 
 void 
@@ -234,12 +193,15 @@ setup()
 {
     Serial.begin(115200);    
     setup_wifi();
-    client.setServer(mqtt_server, 1883);
+    client.setServer(MQTT_SERVER, 1883);
     client.setCallback(callback);
-    SPI.begin();           
-    mfrc522.PCD_Init();   
-    initializeCurrent();
-    timeClient.begin();
+    pinMode(SS_PIN, OUTPUT);
+    SPI.begin();       
+    SPI.setDataMode(SPI_MODE3);    
+    mfrc522.PCD_Init();  
+    delay(4);       
+    mfrc522.PCD_DumpVersionToSerial(); 
+    time_client.begin();
 }
 
 void 
@@ -247,45 +209,50 @@ loop()
 { 
     if (!client.connected()) reconnect();
     client.loop();
-    timeClient.update();
-
+    time_client.update();
+    
     // TODO: só vai ler o RFID quando a porta estiver fechada
-    String rfid_uid = readRFID();
-    if (rfid_uid.length() > 0) {
-        Serial.println("Leitura: " + rfid_uid);
-        if (isAuthorizedID(rfid_uid)) {
-            if (notInCurrent(rfid_uid)) {
-                // Entrada
-                addToCurrent(rfid_uid);
-                String message = "{ \"id\": \"" + rfid_uid + "\", \"timestamp\": \"" + 
-                                timeClient.getFormattedDate() + "\" }";
-                client.publish("entrada", message.c_str());
-                delay(1000);
+    /* Handler da leitura do cartão */
+    char message[MAX_BUFFER_SIZE];
+    readRFID();
+    if (strlen(uid) > 0) {
+        // Serial.printf("Leitura: %s\n", uid);
+        const char *topic;
+        if (list_contains(authorized, uid, &str_cmp)) {
+            snprintf(message, MAX_BUFFER_SIZE, 
+                    "{ \"id\": \"%s\", \"timestamp\": \"%s\" }",
+                    uid, time_client.getFormattedDate().c_str());
+            // Entrada
+            if (!list_contains(current, uid, &str_cmp)) {
+                list_append(current, uid, strlen(uid) + 1);
+                topic = "entrada";
             }
             // TODO: possível bug: pessoa nao autorizada nao pode sair
+            // Saída
             else {
-                // Saída
-                removeFromCurrent(rfid_uid);
-                String message = "{ \"id\": \"" + rfid_uid + "\", \"timestamp\": \"" + 
-                                timeClient.getFormattedDate() + "\" }";
-                client.publish("saida", message.c_str());
-                delay(1000);
+                list_remove(current, uid, &str_cmp);
+                topic = "saida";
             }
         }
         else {
-            String message = rfid_uid;
-            client.publish("desconhecidos", message.c_str());
-            delay(1000);
+            strncpy(message, uid, MAX_BUFFER_SIZE);
+            topic = "desconhecidos";
         }
+
+        client.publish(topic, message);
     }
-  
+
+    /* Envio de mensagens periódicas para o tópico "ocupacao" */
+    static unsigned long last_msg = 0;
     unsigned long now = millis();
-    if (now - lastMsg > 5000) {
-        lastMsg = now;
-        ++value;
-        String message = "{ \"ocupantes\": \"" + formatCurrent() + "\", \"timestamp\": \"" + 
-                              timeClient.getFormattedDate() + "\" }";
-        // Retain
-        client.publish("ocupacao", message.c_str(), 1);
+    if (now - last_msg > 5000) {
+       last_msg = now;
+       char buffer[MAX_BUFFER_SIZE];
+       format_current(buffer);
+       snprintf(message, MAX_BUFFER_SIZE, 
+                "{ \"ocupantes\": \"%s\", \"timestamp\": \"%s\" }",
+                buffer, time_client.getFormattedDate().c_str());
+       // Retain
+       client.publish("ocupacao", message, 1);
     }
 }
